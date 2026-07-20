586d89b570c9976ff8e5ca1e8dea462bd318a0cc
Bobby Carelse <bobbycarelse@gmail.com>
2026-07-20 13:41:16 +0200

    fix: replace sendgrid with bird

--- apps/api/src/routes/v1/contact/contact.ts
+++ apps/api/src/routes/v1/contact/contact.ts
@@ -16,30 +16,38 @@ contactRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: 'Description is required' });
   }
 
-  await sendEmail({
-    to: req?.body?.email,
-    from: 'bobbycarelse@gmail.com',
-    subject: `Thanks ${req?.body?.name}, I've got it`,
-    template: 'contact-confirmation',
-    variables: {
-      first_name: req?.body?.name,
-      message: req?.body?.description,
-    },
-  });
+  try {
+    await sendEmail({
+      to: req?.body?.email,
+      from: 'bobbycarelse@gmail.com',
+      subject: `Thanks ${req?.body?.name}, I've got it`,
+      template: 'contact-confirmation',
+      variables: {
+        first_name: req?.body?.name,
+        message: req?.body?.description,
+      },
+    });
+  } catch (error) {
+    throw new Error('Confirmation Email failed');
+  }
 
-  await sendEmail({
-    to: 'bobbycarelse@gmail.com',
-    from: 'bobbycarelse@gmail.com',
-    subject: 'Peregrine Commit - Notification',
-    template: 'contact-notification',
-    variables: {
-      name: req?.body?.name,
-      email: req?.body?.email,
-      message: req?.body?.description,
-      phoneNumber: req?.body?.phoneNumber ?? 'N/A',
-      hearAboutMe: req?.body?.hearAboutMe ?? 'N/A',
-    },
-  });
+  try {
+    await sendEmail({
+      to: 'bobbycarelse@gmail.com',
+      from: 'bobbycarelse@gmail.com',
+      subject: 'Peregrine Commit - Notification',
+      template: 'contact-notification',
+      variables: {
+        name: req?.body?.name,
+        email: req?.body?.email,
+        message: req?.body?.description,
+        phoneNumber: req?.body?.phoneNumber ?? 'N/A',
+        hearAboutMe: req?.body?.hearAboutMe ?? 'N/A',
+      },
+    });
+  } catch (error) {
+    throw new Error('Notification Email failed');
+  }
 
   return res.status(200).json({ message: 'Contact form submitted successfully' });
 });
